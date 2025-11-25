f8c77be12ae1776f043b9278e5ffe5ac00224d24
Felipe Rudloff <felipe.rudloff@hfsolutions.cl>
2025-11-25 12:18:09 -0300

    Set the font of nvim to be a cursive

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,7 @@
 if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
+if vim.g.neovide then
+  vim.o.guifont = "Maple Mono:h14"  -- Change size as needed
+end
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
