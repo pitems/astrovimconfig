1df346e81c7aa41254cae178f545b07f704440ae
pitems <pipe.rudloff@gmail.com>
2026-08-13 14:34:42 -0400

    Bufferline background color
    Now is color gray looking good

--- lua/plugins/bufferline_conf.lua
+++ lua/plugins/bufferline_conf.lua
@@ -18,6 +18,19 @@ return {
   config = function(_, opts)
     require("bufferline").setup(opts)
 
+    local function blend_bufferline_fill()
+      local separator = vim.api.nvim_get_hl(0, { name = "BufferLineSeparator", link = false })
+      if separator.fg then
+        vim.api.nvim_set_hl(0, "BufferLineFill", { bg = separator.fg })
+      end
+    end
+
+    blend_bufferline_fill()
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      group = vim.api.nvim_create_augroup("bufferline_fill_blend", { clear = true }),
+      callback = blend_bufferline_fill,
+    })
+
     vim.keymap.set("n", "<leader>bt", "<cmd>BufferLineGroupToggle pinned<cr>", {
       desc = "Toggle pinned buffers",
     })
