5dfa24672f5c9f4d72810ab3dcda53fce41882bf
pitems <pipe.rudloff@gmail.com>
2026-08-13 15:55:06 -0400

    Slant colors fixed

--- lua/plugins/bufferline_conf.lua
+++ lua/plugins/bufferline_conf.lua
@@ -18,6 +18,33 @@ return {
   config = function(_, opts)
     require("bufferline").setup(opts)
 
+    local luna_bg = "#060606"
+    pcall(function() luna_bg = require("luna.palette").bg end)
+
+    local function blend_selected_buffers()
+      local selected_groups = {
+        "BufferLineBufferSelected",
+        "BufferLineCloseButtonSelected",
+        "BufferLineModifiedSelected",
+        "BufferLineDuplicateSelected",
+        "BufferLineErrorSelected",
+        "BufferLineWarningSelected",
+        "BufferLineInfoSelected",
+        "BufferLineHintSelected",
+      }
+
+      for _, name in ipairs(selected_groups) do
+        local highlight = vim.api.nvim_get_hl(0, { name = name, link = false })
+        highlight.bg = luna_bg
+        vim.api.nvim_set_hl(0, name, highlight)
+      end
+
+      local separator = vim.api.nvim_get_hl(0, { name = "BufferLineSeparatorSelected", link = false })
+      separator.bg = luna_bg
+      vim.api.nvim_set_hl(0, "BufferLineSeparatorSelected", separator)
+
+    end
+
     local function blend_bufferline_fill()
       local separator = vim.api.nvim_get_hl(0, { name = "BufferLineSeparator", link = false })
       if separator.fg then
@@ -25,10 +52,14 @@ return {
       end
     end
 
+    blend_selected_buffers()
     blend_bufferline_fill()
     vim.api.nvim_create_autocmd("ColorScheme", {
       group = vim.api.nvim_create_augroup("bufferline_fill_blend", { clear = true }),
-      callback = blend_bufferline_fill,
+      callback = function()
+        blend_selected_buffers()
+        blend_bufferline_fill()
+      end,
     })
 
     vim.keymap.set("n", "<leader>bt", "<cmd>BufferLineGroupToggle pinned<cr>", {
